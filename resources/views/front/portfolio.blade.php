361d3df917d773391756b904c0c67b2bb6f874f2
Albert Muhatia <albertmuhatia@gmail.com>
2023-07-27 13:08:13 +0300

    Update Portfolio

--- resources/views/front/portfolio.blade.php
+++ resources/views/front/portfolio.blade.php
@@ -2,11 +2,23 @@
 
 @section('content')
 
-<main>
-    <div class="container">
+<main style="background-color: #ffffff">
+    {{--  --}}
+    <div class="product_versions" style="text-align: center; line-height:normal; background-color: rgb(238,238,240); padding:100px">
+       {{-- <h1>ENJOY PROFESSIONAL AND RELIABLE INSTALLATION SERVICES</h1> --}}
+
+        <h2 class="product_version_h2" style="font-weight: 100; padding-top:100px; margin: 20px 25px;">With great products you need an equally great service!</h2>
+
+        <h3 class="product_version_h3" style="font-weight: 100; max-width:700px; margin:0px auto; color: rgb(87,68,57);">
+            We have dedicated ourselves to perfection. Yes, our products are amazing, but so are our services! Enjoy a hassle free service, from purchase to installation.
+        </h3>
+
+    </div>
+    {{--  --}}
+    <div class="container" style="background-color: #ffffff">
       <!-- Gallery -->
       <section class="section menu" aria-label="menu-label" id="menu">
-        <h2 class="headline-1 section-title text-center">Our Project Gallery</h2>
+        {{-- <h2 class="headline-1 section-title text-center" style="color:#2944d1; text-shadow: 1px 1px 0px #E34C8A;">Our Project Gallery</h2> --}}
         <div class="image-grid">
 
 
