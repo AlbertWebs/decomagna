c1d7976fb14b2cae50e67eaf0181200ae7311180
AlbertWebs <albertmuhatia@gmail.com>
2024-03-13 16:59:22 +0300

    update

--- resources/views/front/footer.blade.php
+++ resources/views/front/footer.blade.php
@@ -1,5 +1,6 @@
 <footer id="pq-footer">
-         <div class="pq-footer-style-1">
+    <div class="pq-footer-style-1" style="background-color:rgba(0, 0, 0,0.3);">
+        <div class="container">
             <div class="pq-subscribe align-items-center">
                 <div class="container">
                     <div class="row align-items-center">
@@ -31,102 +32,103 @@
                 </div>
             </div>
             <div class="pq-footer-top">
-               <div class="container">
-                  <div class="row">
-                     <div class="col-lg-4 col-md-6">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6">
                         <div class="widget">
-                           <h4 class="footer-title">Professional & Reliable Flooring Installation Services</h4>
-                           <p>
+                            <h4 class="footer-title">Professional & Reliable Flooring Installation Services</h4>
+                            <p>
                                 We take our client satisfaction very seriously...  that’s why we have perfected the art of flooring installations with our team of seasoned technicians across Kenya.
-                           </p>
-                           <ul>
-                              <li>
-                                 <a href="tel:+254 79 611 0511"><i class="fas fa-phone-alt"></i>
-                                 <span> &nbsp; &nbsp; +254 79 611 0511</span>
-                                 </a>
-                              </li>
-                              <li>
+                            </p>
+                            <ul>
+                                <li>
+                                    <a href="tel:+254 79 611 0511"><i class="fas fa-phone-alt"></i>
+                                    <span> &nbsp; &nbsp; +254 79 611 0511</span>
+                                    </a>
+                                </li>
+                                <li>
                                 <a href="mailto:info@decomagna.com"><i class="fas fa-envelope"></i>
                                 <span> &nbsp; &nbsp; info@decomagna.com </span>
                                 </a>
-                             </li>
-                           </ul>
+                                </li>
+                            </ul>
                         </div>
-                     </div>
+                        </div>
 
-                     <div class="col-lg-2  col-md-6">
+                        <div class="col-lg-2  col-md-6">
                         <div class="widget">
-                           <h4 class="footer-title">Quick Links</h4>
-                           <div class="menu-product-menu-container">
-                              <ul id="menu-product-menu" class="menu">
+                            <h4 class="footer-title">Quick Links</h4>
+                            <div class="menu-product-menu-container">
+                                <ul id="menu-product-menu" class="menu">
 
-                                 <li class="menu-item">
+                                    <li class="menu-item">
                                     <a href="{{url('/')}}">Home</a>
-                                 </li>
-                                 <li class="menu-item">
+                                    </li>
+                                    <li class="menu-item">
                                     <a href="{{url('/')}}/#about">About Us</a>
-                                 </li>
-                                 <li class="menu-item">
+                                    </li>
+                                    <li class="menu-item">
                                     <a href="{{url('/')}}/portfolio">Our Portfolio</a>
-                                 </li>
+                                    </li>
 
-                              </ul>
-                           </div>
+                                </ul>
+                            </div>
                         </div>
-                     </div>
+                        </div>
 
-                     <div class="col-lg-2  col-md-6">
+                        <div class="col-lg-2  col-md-6">
                         <div class="widget">
-                           <h4 class="footer-title">Our Solutions</h4>
-                           <div class="menu-product-menu-container">
-                              <ul id="menu-product-menu" class="menu">
+                            <h4 class="footer-title">Our Solutions</h4>
+                            <div class="menu-product-menu-container">
+                                <ul id="menu-product-menu" class="menu">
                                 <?php
                                     $Category = DB::table('categories')->get();
                                 ?>
                                 @foreach($Category as $category)
-                                 <li class="menu-item">
+                                    <li class="menu-item">
                                     <a href="{{url('/')}}/products/{{$category->slung}}">{{$category->name}}</a>
-                                 </li>
-                                 @endforeach
-                              </ul>
-                           </div>
+                                    </li>
+                                    @endforeach
+                                </ul>
+                            </div>
                         </div>
-                     </div>
+                        </div>
 
-                     <div class="col-lg-4  col-md-6">
+                        <div class="col-lg-4  col-md-6">
                         <div class="widget widget-port-1">
-                           <h4 class="footer-title">Our showroom</h4>
-                           <div class="row">
-                              <div class="col-sm-12">
-                                 <ul class="pq-contact">
+                            <h4 class="footer-title">Our showroom</h4>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <ul class="pq-contact">
                                     <li>
-                                       <i class="fas fa-map-marker"></i>
-                                       <span>
+                                        <i class="fas fa-map-marker"></i>
+                                        <span>
                                         Tumbili rd. Karen Hardy near the Giraffe Centre off Tumbili rd Navy Blue gate written quick-step , Nairobi, Kenya
-                                       </span>
+                                        </span>
                                     </li>
 
 
-                                 </ul>
-                              </div>
-                           </div>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
-                     </div>
-                  </div>
-               </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="pq-copyright-footer">
-               <div class="container">
-                  <div class="row">
-                     <div class="col-md-12 align-self-center">
-                        <span class="pq-copyright">
-                            Copyright &copy; {{date('Y')}} Quick-Step Flooring Kenya - Decomagna Limited
-                            . All Rights Reserved | Powered by
-                            <a href="https://designekta.com/" target="_blank" class="link">Designekta studios</a> | <a href="#" target="_blank" class="link">Sitemaps</a>
-                        </span>
-                     </div>
-                  </div>
-               </div>
+        </div>
+        <div class="pq-copyright-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 align-self-center">
+                    <span class="pq-copyright">
+                        Copyright &copy; {{date('Y')}} Quick-Step Flooring Kenya - Decomagna Limited
+                        . All Rights Reserved | Powered by
+                        <a href="https://designekta.com/" target="_blank" class="link">Designekta studios</a> | <a href="#" target="_blank" class="link">Sitemaps</a>
+                    </span>
+                    </div>
+                </div>
             </div>
-         </div>
-      </footer>
+        </div>
+    </div>
+</footer>
