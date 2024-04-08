027a2470d8129344ec4057b3462b5197d0f4d0e2
AlbertWebs <albertmuhatia@gmail.com>
2024-04-08 11:13:45 +0300

    Updte

--- resources/views/front/master.blade.php
+++ resources/views/front/master.blade.php
@@ -4,6 +4,7 @@
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
       <!-- Favicon -->
       <link rel="shortcut icon" href="{{asset('uploads/logo/favicon.png')}}">
       <link rel="apple-touch-icon" sizes="180x180" href="{{asset('favicon/apple-touch-icon.png')}}">
