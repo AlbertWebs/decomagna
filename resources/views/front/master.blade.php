2f5700b892fabd531670e20af13f8004dbfaa023
AlbertWebs <albertmuhatia@gmail.com>
2024-04-08 11:11:52 +0300

    Update Apple Touch Icons

--- resources/views/front/master.blade.php
+++ resources/views/front/master.blade.php
@@ -6,6 +6,11 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <!-- Favicon -->
       <link rel="shortcut icon" href="{{asset('uploads/logo/favicon.png')}}">
+      <link rel="apple-touch-icon" sizes="180x180" href="{{asset('favicon/apple-touch-icon.png')}}">
+      <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon/favicon-32x32.png')}}">
+      <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon/favicon-16x16.png')}}">
+      <link rel="manifest" href="{{asset('favicon//site.webmanifest')}}">
+
       <!-- Bootstrap -->
       <link rel="stylesheet" href="{{asset('html/css/bootstrap.min.css')}}">
       <!-- Simplebar -->
